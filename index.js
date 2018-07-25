1536536de3c10683f8500cd3253feff8c2fdeaa4
Snef <38905368+Snefff@users.noreply.github.com>
2018-07-25 09:21:12 +0000

    manual push

---
+++ index.js
@@ -0,0 +1,173 @@
+// dependencies
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('https');
+var unirest = require("unirest");
+let errorResponse = {
+    results: []
+};
+var port = process.env.PORT || 8080;
+var apiKey = '&apiKey=dc7a99af9cc6432e9791af434a6f1328';
+// create serve and configure it.
+const server = express();
+server.use(bodyParser.json());
+
+server.post('/get',function (request,response) {
+  var param = request.body.result;
+  switch(param['test']) {
+    case test1 :
+      console.log('test 1');
+    break;
+    case test2 :
+      console.log('test 2');
+    break;
+    default:
+      console.log('test default');
+  }
+  response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify({
+    "speech" : "Hello from /get :)",
+    "displayText" : "Hello from /get :)"
+  })); 
+})
+
+
+server.post('/getNews',function (request,response)  {
+    if(request.body.result.parameters['top-headline']) {
+        var req = unirest("GET", "https://newsapi.org/v2/top-headline?");
+            req.query({
+                "page": "1" || request.body.result.parameters['page'],
+                "language": fr || request.body.result.parameters['lang'],
+                "category": "general" || request.body.result.parameters['category'],
+                "apiKey": apiKey
+            });
+            req.send("{}");
+            req.end(function(res) {
+                if(res.error) {
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : "Error. Can you try it again ? ",
+                        "displayText" : "Error. Can you try it again ? "
+                    }));
+                } else if(res.body.results.length > 0) {
+                    let article = res.body.article;
+                    let output = '';
+                    for(let i = 0; i<article.length;i++) {
+                        output += article[i].title;
+                        output+="\n"
+                    }
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : output,
+                        "displayText" : output
+                    })); 
+                }
+            });
+    }
+    /* else if(request.body.result.parameters['source-name']) {
+     //   console.log('popular-movies param found');
+        let movie = request.body.result.parameters['source-name'];
+        var req = unirest("GET", "https://api.themoviedb.org/3/search/movie");
+            req.query({
+                "include_adult": "false",
+                "page": "1",
+                "query":movie,
+                "language": "en-US",
+                "api_key": ""
+            });
+            req.send("{}");
+            req.end(function(res) {
+                if(res.error) {
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : "Error. Can you try it again ? ",
+                        "displayText" : "Error. Can you try it again ? "
+                    }));
+                } else if(res.body.results.length > 0) {
+                let result = res.body.results[0];
+                let output = "Average Rating : " + result.vote_average + 
+                "\n Plot : " + result.overview + "url" + result.poster_path
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : output,
+                        "displayText" : output
+                    }));
+                } else {
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : "Couldn't find any deatails. :(  ",
+                        "displayText" : "Couldn't find any deatails. :(  "
+                    }));
+                }
+            });
+
+    } else if(request.body.result.parameters['theme-name']) {    
+        var req = unirest("GET", "https://api.themoviedb.org/3/movie/popular");
+            req.query({
+                "page": "1",
+                "language": "en-US",
+                "api_key": ""
+            });
+            req.send("{}");
+            req.end(function(res){
+                if(res.error) {
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : "Error. Can you try it again ? ",
+                        "displayText" : "Error. Can you try it again ? "
+                    }));
+                } else {
+                    let result = res.body.results;
+                    let output = '';
+                    for(let i = 0; i < result.length;i++) {
+                        output += result[i].title;
+                        output+="\n"
+                    }
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "speech" : output,
+                        "displayText" : output
+                    })); 
+                }
+            });
+    } */
+});
+/*
+server.get('/getNewsName',function (req,res){
+  var req = unirest("GET", "https://newsapi.org/v2/sources?"+apiKey);
+  req.query({
+      "category": request.body.result.parameters['category'],
+      "language": request.body.result.parameters['language'],
+      "country": requesst.body.result.parameters['country']
+  });
+  req.send("{}");
+  req.end(function(res) {
+      if(res.error) {
+        console.log('getNewsName : res error')
+          response.setHeader('Content-Type', 'application/json');
+          response.send(JSON.stringify({
+              "speech" : "Error. Can you try it again ? ",
+              "displayText" : "Error. Can you try it again ? "
+          }));
+      } else if(res.body.results.length > 0) {
+          let result = res.body.results;
+          console.log('result ' + result);
+          let output = '';
+          for(let i = 0; i<result.length;i++) {
+              output += result[i].title;
+              output+="\n"
+          }
+          console.log('output : ' + output);
+          response.setHeader('Content-Type', 'application/json');
+          response.send(JSON.stringify({
+              "speech" : output,
+              "displayText" : output
+          })); 
+      }
+  });  
+}); */
+
+server.listen(port, function () {
+    console.log("Server is up and running...");
+});
